e52be97747b4475820d524cce3223eae0f3a65b9
Christerle Nounoute <christerlesn@gmail.com>
2020-08-11 15:59:10 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,4 @@
 def display_board(the_board) #what's made in a method, stays in a method
-
-
   puts " #{the_board[0]}  | #{the_board[1]}  | #{the_board[2]}  "
   puts "-----------"
   puts " #{the_board[3]}  | #{the_board[4]}  | #{the_board[5]}  "
@@ -8,10 +6,9 @@ def display_board(the_board) #what's made in a method, stays in a method
   puts " #{the_board[6]}  | #{the_board[7]}  | #{the_board[8]}  "
 end #Building a machine.
 
-puts "Welcome to Tic Tac Toe!"
+# puts "Welcome to Tic Tac Toe!"
 
-display_board
+# display_board
 
-puts "First move"
 board = [" "," "," "," ","X"," "," "," "," "]
-display_board(the_board)
+display_board(board)
