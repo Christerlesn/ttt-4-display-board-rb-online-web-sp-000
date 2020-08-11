4a306d7cafd8a46c67395e84fad05fb881dbde0f
Christerle Nounoute <christerlesn@gmail.com>
2020-08-11 15:59:28 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,5 @@
 def display_board(the_board) #what's made in a method, stays in a method
-  puts " #{the_board[0]}  | #{the_board[1]}  | #{the_board[2]}  "
+  puts "#{the_board[0]}| #{the_board[1]}  | #{the_board[2]}  "
   puts "-----------"
   puts " #{the_board[3]}  | #{the_board[4]}  | #{the_board[5]}  "
   puts "-----------"
